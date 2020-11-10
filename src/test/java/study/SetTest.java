63985820f44037d9b98bc524b9cd6e508e55e47a
이후민 <lhm7877>
2020-11-10 16:29:10 +0900

    feat(SetTest): add contains

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,11 +2,16 @@ package study;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.jupiter.common.util.Strings;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -24,5 +29,17 @@ public class SetTest {
     public void size() {
         assertThat(numbers)
                 .hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    public void contains(int input){
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input) {
+        assertTrue(Strings.isBlank(input));
     }
 }
