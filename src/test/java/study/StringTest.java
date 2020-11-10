d95334889aa9efc705032ddf3d20c6530ca642f6
이후민 <lhm7877>
2020-11-10 16:00:25 +0900

    feat: add charAt test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,21 +1,27 @@
 package study;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 
 public class StringTest {
     @Test
     public void splitTest() {
-        final String test1 = "1,2";
-        final String[] split1 = test1.split(",");
-        assertThat(split1)
-                .contains("1","2");
+        {
+            final String test = "1,2";
+            final String[] splitString = test.split(",");
+            assertThat(splitString)
+                    .contains("1", "2");
+        }
 
-        final String test2 = "1";
-        final String[] split2 = test2.split(",");
-        assertThat(split2)
-                .containsExactly("1");
+        {
+            final String test = "1";
+            final String[] splitString = test.split(",");
+            assertThat(splitString)
+                    .containsExactly("1");
+        }
     }
 
     @Test
@@ -24,5 +30,24 @@ public class StringTest {
         final String substring = test.substring(1, 4);
         assertThat(substring)
                 .isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 테스트")
+    public void charAtTest() {
+        {
+            final String test = "abc";
+            final char charAtString = test.charAt(1);
+            assertThat(charAtString)
+                    .isEqualTo('b');
+        }
+
+        {
+            assertThatThrownBy(() -> {
+                final String test = "ab";
+                test.charAt(2);
+            }).isInstanceOf(IndexOutOfBoundsException.class)
+                    .hasMessageContaining("index out of range: 2");
+        }
     }
 }
